e488fcba4178e88deb15d158117e15948f8e9312
Divyanshushastri <divyanshushastri96@gmail.com>
2021-08-26 23:23:49 +0530

    fourth

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
 let hasBlackjack = false
+let isAlive = true
+let message = ""
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
+    message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    message = "Wohoo! You've got Blackjack! 🥳"
     hasBlackjack =true
 } else  {
-    console.log("You're out of the game! 😭")
+    message = "You're out of the game! 😭"
+    isAlive = false
 }
+console.log(hasBlackjack)
+console.log(isAlive)
+console.log(message)
