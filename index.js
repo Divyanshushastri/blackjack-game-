1b425d3630eef27e74b6a0b3bab11d579e1887e5
Divyanshushastri <divyanshushastri96@gmail.com>
2021-08-26 23:05:20 +0530

    third

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
+let hasBlackjack = false
 
 if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackjack =true
 } else  {
     console.log("You're out of the game! 😭")
 }
