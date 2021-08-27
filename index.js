73b01d7628dfc29e922218b63dffdf02dcb32667
Divyanshushastri <divyanshushastri96@gmail.com>
2021-08-27 07:42:18 +0530

    beforesleep

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 let firstCard = 6
 let secondCard = 9
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
 let message = ""
+let sumEl = document.getElementById("sum-el")
+let messageEl = document.getElementById("message-el")
+let cardEl = document.getElementById("cards-el")
 
 function startGame() {
+    renderGame()
+}
+function renderGame() {
+    sumEl.textContent = "Sum: " + sum
+    cardEl.textContent = "Cards: " + cards[0] + " " + cards[1]
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card? "
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = " You've got Blackjack! "
         hasBlackjack =true
     } else  {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game! "
         isAlive = false
     }
-    console.log(hasBlackjack)
-    console.log(isAlive)
-    console.log(message)
+    messageEl.textContent = message
+
+}function newCard() {
+    let card = 6
+    
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
+
 }
