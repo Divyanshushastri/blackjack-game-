20cb73aecc05e4534d6242f44bf222f183cc9d23
Divyanshushastri <divyanshushastri96@gmail.com>
2021-09-03 08:08:01 +0530

    newhope

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-let firstCard = 6
-let secondCard = 9
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
-let hasBlackjack = false
-let isAlive = true
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
 let message = ""
 let sumEl = document.getElementById("sum-el")
 let messageEl = document.getElementById("message-el")
 let cardEl = document.getElementById("cards-el")
 
+function getRandomCard(){
+    let randomNumer = Math.floor( Math.random() * 13 ) + 1
+
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
+} 
+
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
     renderGame()
 }
 function renderGame() {
+    cardEl.textContent = "Cards: "
+    
+    for (let i = 0; i < cards.length; i++) {
+        cardEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
-    cardEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+  
     if (sum <= 20) {
         message = "Do you want to draw a new card? "
     } else if (sum === 21) {
@@ -27,7 +48,7 @@ function renderGame() {
     messageEl.textContent = message
 
 }function newCard() {
-    let card = 6
+    let card =  getRandomCard()
     
     sum += card
     cards.push(card)
